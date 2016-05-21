eaac4fb9efa2279ca58ae71ff20d549cef430e3a
kdylan <kdylan@qq.com>
2016-05-21 19:22:50 +0800

    基本功能实现

--- src/SRouter.js
+++ src/SRouter.js
@@ -2,9 +2,10 @@
  * SRouter
  * author: dylan<kdylan@qq.com>
  */
-'use strict'
+
 
 (function (window) {
+    'use strict'
     window.SRouter = {
         routers: [], // 路由规则存储
         /**
@@ -37,7 +38,8 @@
                 paramsName: route.paramsName,
                 name: name,
                 hander: hander
-            })
+            });
+            return this;
         },
         /**
          * 删除路由
@@ -49,36 +51,80 @@
             for(var i = 0, len = routers.length; i < len; i++) {
                 if(routers[i].name === name) {
                     routers.splice(i,1);
-                    return true;
+                    break;
                 }
-            }
+            };
+            return this;
         },
         /**
          * 开始监听路由
          */
         listen: function () {
+            var self = this;
             // 绑定监听事件            
             if(this.mode === 'history'){
                 this._addEvent(window, 'popstate', function () {
-                    
-                })
+                    var path = location.path;
+                    self._check(path);
+                });
             } else {
                 this._addEvent(window, 'hashchange', function () {
-                    
-                })
+                    var path = location.hash.replace(/^#/,'');
+                    self._check(path);
+                });
             } 
+            return this;
         },
         /**
          * 检查路由是否匹配
-         * - router String 路由规则
          * - path String 路径
          */
-        _check: function (router, path) {
-            
+        _check: function (path) {
+            var routers = this.routers;
+            for(var i = 0,len = routers.length; i < len; i++) {
+                var match = path.match(routers[i].reg);
+                // 匹配成功
+                if(match) {
+                    var params = {};
+                    // 组合参数
+                    routers[i].paramsName && routers[i].paramsName
+                        .forEach(function (name,index) {
+                            params[name] = match[index + 1];
+                        });
+                   
+                    routers[i].hander.call({}, {
+                        params: params 
+                    });
+                    return true;
+                }
+            }
+            // 都不匹配时，如果有默认函数则执行
+            this.defaultHander && this.defaultHander.call({});
+        },
+        /**
+         * 跳转到路由
+         * - path
+         */
+        go: function (path) {
+            if(this.mode === 'history') {
+                window.history.pushState({
+                    path: path
+                },'',path);
+                this._check(path);
+            } else {
+                window.location.hash = path;
+            }
+        },
+        /**
+         * 默认函数 如果路由都不匹配时执行
+         */
+        default: function (hander) {
+            this.defaultHander = hander;
+            return this;
         },
         /**
          * 解析路由，将params形式转换为正则表达式
-         * - router String路由规则
+         * - router String 路由规则
          */
         _compile: function (router) {
             var route = {}; // 路由对象
@@ -86,12 +132,16 @@
             // 将/post/:postid 转换为 /post/(\w+);
             var paramsReg = /:(\w+)/g;
             var paramsMatch = router.match(paramsReg);
+            
             if(paramsMatch) {
-                route.paramsName = paramsMatch.shift();
+              
+                route.paramsName = paramsMatch.map(function (name) {
+                    return name.replace(':','')
+                });
                 router = router.replace(/:(\w+)/g, '(\\w+)');
             }
             
-            route.reg = new RegExp(router);
+            route.reg = new RegExp( '^' + router + '$');
             return route;
         },
         /**
