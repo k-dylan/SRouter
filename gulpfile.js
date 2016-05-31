2abc5043d5adbca697e362a98435c31f9dadfdf9
kdylan <kdylan@qq.com>
2016-05-31 21:53:31 +0800

    压缩源代码

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 
 gulp.task('server', function () {
@@ -8,3 +10,12 @@ gulp.task('server', function () {
         root: './'
     });
 });
+
+gulp.task('build', function () {
+    return gulp.src('./src/SRouter.js')
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+        }))
+        .pipe(gulp.dest('./src'));
+})
